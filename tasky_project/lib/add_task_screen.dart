f6ed9e57f21bd0876bc7c9a0e0eba5d650f66053
Monica Saeed <monicasaeed12@gmail.com>
2025-05-29 20:23:58 +0300

    add validation to task name
    task name is required

--- tasky_project/lib/add_task_screen.dart
+++ tasky_project/lib/add_task_screen.dart
@@ -9,72 +9,89 @@ class AddTaskScreen extends StatefulWidget {
 }
 
 class _AddTaskScreenState extends State<AddTaskScreen> {
+  final TextEditingController taskNameController = TextEditingController();
+  final TextEditingController taskDescriptionController =
+      TextEditingController();
+  final GlobalKey<FormState> _key = GlobalKey<FormState>();
+
   bool isHighPriority = true;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 50),
-            Row(
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _key,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                IconButton(
-                  icon: Icon(Icons.arrow_back, color: Colors.white),
-                  onPressed: () {
-                    Navigator.pop(context);
+                Row(
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.arrow_back, color: Colors.white),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                    SizedBox(width: 4),
+                    Text(
+                      'New Task',
+                      style: Theme.of(context).textTheme.displayMedium,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 18),
+                CustomFormField(
+                  title: 'Task Name',
+                  hintText: 'Finish UI design for login screen',
+                  maxLines: 1,
+                  controller: taskNameController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a task name';
+                    }
+                    return null;
                   },
                 ),
-                SizedBox(width: 4),
-                Text(
-                  'New Task',
-                  style: Theme.of(context).textTheme.displayMedium,
+                SizedBox(height: 20),
+                CustomFormField(
+                  title: 'Task Description',
+                  hintText:
+                      'Finish onboarding UI and hand off to devs by Thursday.',
+                  maxLines: 5,
+                  controller: taskDescriptionController,
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'High Priority  ',
+                      style: Theme.of(context).textTheme.displaySmall,
+                    ),
+                    Switch(
+                        value: isHighPriority,
+                        onChanged: (bool newValue) {
+                          setState(() {
+                            isHighPriority = newValue;
+                          });
+                        }),
+                  ],
+                ),
+                SizedBox(height: 90),
+                ElevatedButton.icon(
+                  icon: Icon(Icons.add, size: 18),
+                  onPressed: () {
+                    if (_key.currentState?.validate() ?? false) {
+                      // Add task logic here
+                    }
+                  },
+                  label: Text('Add Task'),
                 ),
               ],
             ),
-            SizedBox(height: 18),
-            CustomFormField(
-              title: 'Task Name',
-              hintText: 'Finish UI design for login screen',
-              maxLines: 1,
-              controller: TextEditingController(),
-            ),
-            SizedBox(height: 20),
-            CustomFormField(
-              title: 'Task Description',
-              hintText:
-                  'Finish onboarding UI and hand off to devs by Thursday.',
-              maxLines: 5,
-              controller: TextEditingController(),
-            ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'High Priority  ',
-                  style: Theme.of(context).textTheme.displaySmall,
-                ),
-                Switch(
-                    value: isHighPriority,
-                    onChanged: (bool newValue) {
-                      setState(() {
-                        isHighPriority = newValue;
-                      });
-                    }),
-              ],
-            ),
-            SizedBox(height: 90),
-            ElevatedButton.icon(
-              icon: Icon(Icons.add, size: 18),
-              onPressed: () {
-                // Add task logic here
-              },
-              label: Text('Add Task'),
-            ),
-          ],
+          ),
         ),
       ),
     );
